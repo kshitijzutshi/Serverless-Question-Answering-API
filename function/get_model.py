acbafbf08f3b10f0cbdbc20ee20ae476f82726d3
kshitij zutshi <zutshi.k@northeastern.edu>
2022-04-10 14:58:19 -0400

    changes

--- function/get_model.py
+++ function/get_model.py
@@ -4,7 +4,7 @@ def get_model(model):
   """Loads model from Hugginface model hub"""
   try:
     model = AutoModelForQuestionAnswering.from_pretrained(model)
-    model.save_pretrained('./model')
+    model.save_pretrained('../model')
   except Exception as e:
     raise(e)
 
@@ -12,9 +12,9 @@ def get_tokenizer(tokenizer):
   """Loads tokenizer from Hugginface model hub"""
   try:
     tokenizer = AutoTokenizer.from_pretrained(tokenizer)
-    tokenizer.save_pretrained('./model')
+    tokenizer.save_pretrained('../model')
   except Exception as e:
     raise(e)
 
-get_model('hoanhkhoa/roberta-base-finetuned-ner')
-get_tokenizer('hoanhkhoa/roberta-base-finetuned-ner')
+get_model('mrm8488/mobilebert-uncased-finetuned-squadv2')
+get_tokenizer('mrm8488/mobilebert-uncased-finetuned-squadv2')
